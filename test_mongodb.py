267e1bdf857f4397e97202dfe75cb4c7f2b6cfb4
Abhijit Paul <abhijitpaul0212@gmail.com>
2023-05-12 17:09:48 +0530

    First Commit

---
+++ test_mongodb.py
@@ -0,0 +1,42 @@
+from pymongo.mongo_client import MongoClient
+
+uri = "mongodb+srv://root:root@cluster0.k3s4vuf.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+data = {
+    "name": "mousumi",
+    "email": "mousumipaul0103@gmail.com",
+    "surname": "paul"
+    }
+
+# Send a ping to confirm a successful connection
+try:
+    status = client.admin.command('ping')
+    print("Response: {}".format(status))
+    
+    db1 = client.mongotest1  # Here mongotest1 is the name of the database
+    print("Database name is {0}".format(db1.name))
+    coll1 = db1.test  # Here test is the name of table
+    coll1.insert_one(data)
+
+
+    db2 = client['mongotest2']
+    coll2 = db2['test']
+    
+    coll2.insert_one(data).inserted_id
+    coll2.insert_one({'a': 1, 'b': 2}).inserted_id
+
+    print(client.list_database_names())
+    print(db1.list_collection_names())
+    print(db2.list_collection_names())
+
+    print("Done")
+    
+    
+    # print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+    
+    
